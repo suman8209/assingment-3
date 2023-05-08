96417c5456b161ada96be34d6f6e98db41772c0d
Suman Kanwar <124556522+suman8209@users.noreply.github.com>
2023-05-08 13:07:56 +0530

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,22 @@
+
+
+var heading=document.getElementsByTagName("h1")[0].innerHTML="suman";
+var heading=document.getElementsByTagName("h3")[0].innerHTML="chohan."
+
+
+var a;
+a=document.getElementById("box2").innerText="!!! ROHIT JAIN !!! ";
+console.log(a);
+
+
+
+var a=document.getElementsByClassName("box3");
+for (var i = 0; i <a.length; i++) {
+a[i].innerHTML="<h1>Full Stack Learning My traning centre.</h1>";
+}
+
+var a;
+a=document.querySelector(".box4 h1")
+a.innerHTML="!!disha!!";
+a=document.querySelector(".box4 p")
+a.innerHTML="Suman chohan."
